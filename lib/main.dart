8d441776fd2a51debfac0f137271cbb365b25cee
dmil148 <ddmiller7702@gmail.com>
2025-02-13 16:32:45 -0600

    Adjusted AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Abby M and Thomas W's App")),
+      appBar: AppBar(title: const Text("Abby M and Thomas W and Dylan Miller's App")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
