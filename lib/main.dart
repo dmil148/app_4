fd50f63d82422abaf1a6d791e82c36c339f01f00
dmil148 <ddmiller7702@gmail.com>
2025-02-13 16:09:30 -0600

    Adjusted Title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Abby M and Thomas W\'s App',
+      title: 'Abby M and Thomas W and Dylan M\'s App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
